2b71f5b6f52aeca11071ae689b3eb09ff72640ba
mrunal-selokar <mrunal_selokar@outlook.com>
2017-02-01 11:55:02 +0530

    gravatar added

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
  
   resources :hotels
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
 end
