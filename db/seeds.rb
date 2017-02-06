6b47dcc943b1d43106146c582f5a02c3c9d270c9
mrunal-selokar <mrunal_selokar@outlook.com>
2017-02-06 16:57:10 +0530

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+User.create!(name:  "Admin User",
+             email: "admin@email.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+
+
 99.times do |n|
 	name = Faker::Name.name
 	email = "example-#{n+1}@railstutorial.org"
