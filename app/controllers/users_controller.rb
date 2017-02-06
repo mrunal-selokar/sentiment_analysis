cee9c638625dcb9889487b2f765d67b3167abeb4
mrunal-selokar <mrunal_selokar@outlook.com>
2017-02-06 16:09:15 +0530

    edit users added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   
-  before_action :logged_in_user, only: [:index, :destroy]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  
   def show
   	@user = User.find(params[:id])
     @review = @user.reviews.build if logged_in?
     @reviews = @user.reviews.paginate(page: params[:page])
     @feed_items = @user.feed.paginate(page: params[:page])
-
   end
 
   def index
@@ -29,6 +30,21 @@ class UsersController < ApplicationController
   	end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
@@ -43,6 +59,11 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)        
     end
       
 end
