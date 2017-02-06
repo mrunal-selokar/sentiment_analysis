6dede2cc49614a018b75dd9cc465488ab4b6b8ef
mrunal-selokar <mrunal_selokar@outlook.com>
2017-02-06 17:53:21 +0530

    new hotels can be added by admin

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -12,4 +12,26 @@ class HotelsController < ApplicationController
   	@reviews = @hotel.reviews.paginate(page: params[:page])
     @feed_items = @hotel.feed.paginate(page: params[:page])
   end
+
+
+  def new
+    @hotel = Hotel.new
+
+  end
+
+  def create
+    @hotel = Hotel.new(hotel_params)
+    @hotel.total_score = 0.0
+    if @hotel.save
+      flash[:success] = "New hotel added!"
+      redirect_to hotels_path
+    end
+  end
+
+
+ private
+    def hotel_params
+      params.require(:hotel).permit(:name, :city)
+    end
+
 end
