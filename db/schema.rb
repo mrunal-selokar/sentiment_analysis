e2ed2224ccca375ebb2923686e32be0f7118ff66
mrunal-selokar <mrunal_selokar@outlook.com>
2017-02-24 13:50:46 +0530

    amenities table dropped

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213060917) do
-
-  create_table "amenities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.boolean  "wifi"
-    t.boolean  "tv"
-    t.boolean  "pool"
-    t.boolean  "room_service"
-    t.boolean  "ac"
-    t.boolean  "food"
-    t.boolean  "parking"
-    t.boolean  "service"
-    t.integer  "hotel_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["hotel_id"], name: "index_amenities_on_hotel_id", using: :btree
-  end
+ActiveRecord::Schema.define(version: 20170224081923) do
 
   create_table "hotels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -60,7 +45,6 @@ ActiveRecord::Schema.define(version: 20170213060917) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
-  add_foreign_key "amenities", "hotels"
   add_foreign_key "reviews", "hotels"
   add_foreign_key "reviews", "users"
 end
